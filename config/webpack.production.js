14cac3a6b7188194d23a3aad6fa1788afd584284
herding-wolf <herding.wolf.lc@gmail.com>
2020-05-18 15:06:30 +0800

    添加 esm打包模块

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -2,11 +2,17 @@ const baseConfig = require('./webpack.base');
 const meger = require('webpack-merge');
 const webpack = require('webpack');
 const path = require('path');
-
+const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
 const prodConf = meger(baseConfig, {
   entry: {
     index: path.resolve(__dirname, '../src/index.ts'),
   },
+  output: {
+      library:"cyjh_soft",
+      libraryTarget:"var",
+  },
   mode: 'production',
+  // 生成esModule 文件
+  plugins:[new EsmWebpackPlugin()]
 });
 module.exports = prodConf;
