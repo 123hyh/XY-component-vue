d06f06015fa1c1ab0b69dbd3d8e9b9b197d56192
herding-wolf <herding.wolf.lc@gmail.com>
2020-05-22 18:04:59 +0800

    no message

--- src/LongListTest.tsx
+++ src/LongListTest.tsx
@@ -1,7 +1,11 @@
 
 import { componentFactory } from 'vue-tsx-support';
 export default componentFactory.create( {
-  props: {
+  props:{
+    pageSize: {
+      type: Number,
+      default: 100
+    },
     list: {
       type: Array,
       default: () => Array.from( { length: 10000 } ).map( ( _, i ) => i )
@@ -11,9 +15,9 @@ export default componentFactory.create( {
     const ob = new IntersectionObserver( ( [ entris ] )=>{
       const { isIntersecting:visible, intersectionRatio: ratio } = entris;
       // 上滚动时
-      if ( visible === false && ratio !==0 &&  ratio <= 0.2 ) {
+      if ( visible === false && ratio !== 0 &&  ratio <= 0.2 ) {
         console.log( '向上滚动' );
-      } else if ( visible === true && ratio >=0.8 ) {
+      } else if ( visible === true && ratio >= 0.8 ) {
         console.log( '向下滚动' );
       }
     }, {
@@ -23,12 +27,21 @@ export default componentFactory.create( {
     const elem = this.$refs['container'] as Element;
 
     ob.observe( elem );
+    this.currentList =  this.list.slice( 
+      ( this.pageIndex - 1 ) * this.pageSize, this.pageSize * this.pageIndex 
+    );
+  },
+  data() {
+    return {
+      pageIndex: 1,
+      currentList: [],
+    };
   },
   render() {
     return  <div class="xy-long-list-box">
       <div class="xy-long-list-container" ref='container'>
         {
-          this.list.map( ( item )=>{
+          this.currentList.map( ( item )=>{
             const key = item as number;
             return <div key={key}>{item}</div>;
           } )
